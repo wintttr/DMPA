817a71f3240c245b6a97ab32e66a246646c13c79
wintttr <wintttergewitter@gmail.com>
2023-09-12 18:05:18 +0300

    Исправил блюр

--- LR1/drawplus.py
+++ LR1/drawplus.py
@@ -11,11 +11,15 @@ def draw_plus(img, color, thickness=2, blurness=True):
     v_ul = tuple(map(int, (w * 0.45, h * 0.1)))
     v_dr = tuple(map(int, (w * (1 - 0.45), h * (1 - 0.1))))
 
-    cv2.rectangle(img, v_ul, v_dr, color, thickness)
-
+    blurred = None
+    
     if blurness:
         blurred = img[h_ul[1]:h_dr[1], h_ul[0]:h_dr[0]]
         blurred = cv2.blur(blurred, (15, 15))
+    
+    cv2.rectangle(img, v_ul, v_dr, color, thickness)
+    
+    if blurness:
         img[h_ul[1]:h_dr[1], h_ul[0]:h_dr[0]] = blurred
-
+    
     cv2.rectangle(img, h_ul, h_dr, color, thickness)
